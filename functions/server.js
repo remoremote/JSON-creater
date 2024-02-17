178563da2a68eca8b36f586ab114c6e7f2f98bc7
remoremote <81964236+remoremote@users.noreply.github.com>
2024-02-17 16:10:39 +0000

    setting up server funtions

---
+++ functions/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/cronjob-log', (req, res) => {
+  fs.readFile('cronjob.log', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading log file:', err);
+      res.status(500).send('Error reading log file');
+    } else {
+      const lines = data.trim().split('\n');
+      const latestLogs = lines.slice(-10);
+      const formattedLogs = latestLogs.join('\n');
+      res.send(formattedLogs);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
